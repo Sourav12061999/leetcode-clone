37ba0756794d2e65300e813247122ddea2da01c7
Sourav12061999 <sdunity12061999@gmail.com>
2023-05-21 12:54:59 +0530

    chnaged next to react

--- client/src/context/index.tsx
+++ client/src/context/index.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import AuthContextProvider from "./Auth";
 import { ContextProps } from "@/Types";
+import { AuthContext } from "./Auth";
 export default function Context({ children }: ContextProps) {
   return (
     <Fragment>
@@ -8,3 +9,5 @@ export default function Context({ children }: ContextProps) {
     </Fragment>
   );
 }
+
+export { AuthContext };
